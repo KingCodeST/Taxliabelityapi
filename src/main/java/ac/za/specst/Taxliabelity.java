28dd0b1b67bcba7950c65005744943a48df4008f
kingcodest <cyfotyasi@gmail.com>
2021-11-25 19:40:00 -0800

    fixed launch

--- src/main/java/ac/za/specst/Taxliabelity.java
+++ src/main/java/ac/za/specst/Taxliabelity.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Taxliabelityapplication {
+class Taxliabelityapplication {
 
     public static void main(String[] args){
-        SpringApplication.run(Tgit)
+        SpringApplication.run(Taxliabelityapplication.class, args);
     }
 
 
