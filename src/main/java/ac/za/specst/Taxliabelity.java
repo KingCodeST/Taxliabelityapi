acca2e0fe15316a290364bb172695c135d561196
kingcodest <cyfotyasi@gmail.com>
2021-11-25 18:46:05 -0800

    create taxliabelityapi project

---
+++ src/main/java/ac/za/specst/Taxliabelity.java
@@ -0,0 +1,14 @@
+package ac.za.specst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Taxliabelityapplication {
+
+    public static void main(String[] args){
+        SpringApplication.run(Tgit)
+    }
+
+
+}
