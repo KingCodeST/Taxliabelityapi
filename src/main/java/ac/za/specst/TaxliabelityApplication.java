d8eb67fa358e0609e1ec52574751553726d237ab
kingcodest <Tya@2017>
2022-06-26 05:07:14 +0200

    data connections

--- src/main/java/ac/za/specst/TaxliabelityApplication.java
+++ src/main/java/ac/za/specst/TaxliabelityApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-class TaxliabelityApplication {
+public class TaxliabelityApplication {
 
     public static void main(String[] args){
         SpringApplication.run(TaxliabelityApplication.class, args);
